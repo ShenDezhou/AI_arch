010a5b20cb447c6fe6f0efb7bca7e5160fa8fe9b
ShenDezhou <bangtech@sina.com>
2020-11-26 12:16:30 +0800

    1.doc to vec using word2vec.

--- A-BasicCommom/doc2vec/bert_vector.py
+++ A-BasicCommom/doc2vec/bert_vector.py
@@ -6,14 +6,15 @@ from tqdm import tqdm
 
 model = KeyedVectors.load_word2vec_format('model/word2vec.txt', binary=False)
 df = pandas.read_csv("data/summary.csv", sep=',', names=['gid','summary','sort'])
-adf = df[df.summary!='']
+adf = df[df.summary.notnull()]
 total = len(adf)
+print(total)
 dim = model.wv.vector_size
 vocab = set(model.wv.vocab.keys())
-with open("model/summary-512.txt", 'w', encoding='utf-8') as file:
+with open("modelsummary/summary-512.txt", 'w', encoding='utf-8') as file:
   file.write(str(total) + " " + str(dim)+'\n')
 
-  for row in tqdm(df.itertuples(index=False)):
+  for row in tqdm(adf.itertuples(index=False)):
     gid = str(row[0])
     summary = str(row[1])
     words = lawa.lcut(summary)
@@ -22,7 +23,8 @@ with open("model/summary-512.txt", 'w', encoding='utf-8') as file:
       docvec = numpy.mean(vecs, axis=0)
     else:
       docvec = model['。']
-    file.write(gid + " " + numpy.array2string(docvec, separator=' ', max_line_width=10 ** 10, precision=8,
-                                                floatmode='fixed', suppress_small=True).lstrip('[').rstrip(']') + '\n')
+    vecline = (gid + ' '+ numpy.array2string(docvec, separator=' ', max_line_width=10 ** 10, precision=8,
+                                                floatmode='fixed', suppress_small=True).strip('[]') + '\n').replace("  ", " ", 10**10)
+    file.write(vecline)
     #print('.')
 
